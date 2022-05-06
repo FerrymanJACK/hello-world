8098733b6f44e2a21d623ca4746bb81a1fcab11b
FerrymanJACK <wangrongqingJACK@163.com>
2022-05-06 23:32:39 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package org.beta;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("The second change comes from origin!");
         System.out.println("The change comes from origin!");
         System.out.println("Hello world!");
     }
