1703b5c59b7251ca825be0c6f3fd793d8772e787
wangrongqing <wangrongqingjack@163.com>
2022-05-09 10:53:43 +0800

    sixth main

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ package org.beta;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("The sixth change comes from origin!");
         System.out.println("The fifth change comes from origin!");
         System.out.println("The fourth change comes from origin!");
         System.out.println("The third change comes from origin!");
